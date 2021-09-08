09577fc735cdffd75be8c5df79d90eaeae463a86
郭浩东 <guohaodong@sogou-inc.com>
2021-09-08 16:59:39 +0800

    fix

--- conf/conf.go
+++ conf/conf.go
@@ -12,7 +12,7 @@ import (
 // Init 初始化配置项
 func Init() {
 	// 从本地读取环境变量
-	godotenv.Load()
+	godotenv.Load(".env.example")
 
 	// 设置日志级别
 	util.BuildLogger(os.Getenv("LOG_LEVEL"))
@@ -23,6 +23,8 @@ func Init() {
 	}
 
 	// 连接数据库
+	// sql := os.Getenv("MYSQL_DSN")
+	// fmt.Printf("sql=%s", sql)
 	model.Database(os.Getenv("MYSQL_DSN"))
 	cache.Redis()
 }
