16145fc0af9da0181e554ab044611e55a6f84fe0
dannygguo <dannygguo@tencent.com>
2022-05-10 16:49:09 +0800

    增加佣金表

---
+++ entity/table/commission.go
@@ -0,0 +1,19 @@
+package table
+
+import (
+	"time"
+)
+
+type Commission struct {
+	Id        int64     `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT" json:"id"`
+	OrderId   int64     `gorm:"column:order_id;type:bigint(20);NOT NULL" json:"order_id"`
+	UserId    int64     `gorm:"column:user_id;type:bigint(20);NOT NULL" json:"user_id"`     // 佣金所有人id
+	Change    float64   `gorm:"column:change;type:double;default:0;NOT NULL" json:"change"` // 费用变化
+	Type      int       `gorm:"column:type;type:int(11);default:0;NOT NULL" json:"type"`    // 类型：0：分享人下单，1:体现
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"updated_at"`
+}
+
+func (m *Commission) TableName() string {
+	return "commission"
+}
--- entity/table/user.go
+++ entity/table/user.go
@@ -2,15 +2,16 @@ package table
 
 import "time"
 
-// User 用户模型
+// User 用户
 type User struct {
-	ID        int64     `gorm:"column:id" db:"id" json:"id" form:"id"`                                 //  每个用户唯一的userid
-	Email     string    `gorm:"column:email" db:"email" json:"email" form:"email"`                     //  邮箱信息，非空且唯一
-	UserId    string    `gorm:"column:user_id" db:"user_id" json:"user_id" form:"user_id"`             //  用户id-备用的唯一id
-	ChannelId int64     `gorm:"column:channel_id" db:"channel_id" json:"channel_id" form:"channel_id"` //  渠道id，默认为0
-	Source    string    `gorm:"column:source" db:"source" json:"source" form:"source"`                 //  来源：android
-	CreatedAt time.Time `gorm:"column:created_at" db:"created_at" json:"created_at" form:"created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at" db:"updated_at" json:"updated_at" form:"updated_at"`
+	Id        int64     `gorm:"column:id;type:bigint(20);primary_key;AUTO_INCREMENT" json:"id"`         // 每个用户唯一的userid
+	Email     string    `gorm:"column:email;type:varchar(45);NOT NULL" json:"email"`                    // 邮箱信息，非空且唯一
+	UserId    string    `gorm:"column:user_id;type:varchar(45);NOT NULL" json:"user_id"`                // 用户id-备用的唯一id
+	ChannelId int64     `gorm:"column:channel_id;type:bigint(20);NOT NULL" json:"channel_id"`           // 渠道id，默认为0
+	InviterId int64     `gorm:"column:inviter_id;type:bigint(20);default:0;NOT NULL" json:"inviter_id"` // 邀请人id
+	Source    string    `gorm:"column:source;type:varchar(45);NOT NULL" json:"source"`                  // 来源：Android
+	CreatedAt time.Time `gorm:"column:created_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at;type:datetime;default:CURRENT_TIMESTAMP;NOT NULL" json:"updated_at"`
 }
 
 // TableName 会将 User 的表名重写为 `user`
