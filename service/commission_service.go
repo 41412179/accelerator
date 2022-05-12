dc823c81c833e75d11039e024eba5b839cb68424
dannygguo <dannygguo@tencent.com>
2022-05-12 14:44:16 +0800

    添加佣金

--- service/commission_service.go
+++ service/commission_service.go
@@ -6,14 +6,27 @@ import (
 	"accelerator/entity/table"
 	"accelerator/mysql"
 	"accelerator/util"
+
+	"github.com/gin-gonic/gin"
 )
 
+// CommissionService 佣金服务
 type CommissionService struct {
 	user *table.User
 }
 
 // GetCommissionByUser 获取佣金
-func (c *CommissionService) GetCommissionByUser() response.Response {
+func (c *CommissionService) GetCommissionByUser(g *gin.Context) response.Response {
+	user := util.GetUserByCtx(g)
+	if user == nil {
+		util.Log().Error("get user by ctx failed")
+		return response.Response{
+			Code: errcode.CodeCheckLogin,
+			Msg:  errcode.Text(errcode.CodeCheckLogin),
+		}
+	}
+	c.user = user
+
 	commissions, err := mysql.GetCommissionsByUser(c.user.ID)
 	if err != nil {
 		util.Log().Error("get commission by user failed, err: %v", err)
