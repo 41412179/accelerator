40262c4a23c1195ed7b45f2526fbe70d165f677e
dannygguo <dannygguo@tencent.com>
2022-05-25 15:24:50 +0800

    fix bug

--- middleware/cors.go
+++ middleware/cors.go
@@ -12,9 +12,11 @@ func Cors() gin.HandlerFunc {
 	config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Cookie"}
 	if gin.Mode() == gin.ReleaseMode {
 		// 生产环境需要配置跨域域名，否则403
-		config.AllowOrigins = []string{"*"}
+		config.AllowOrigins = []string{"http://apitest.ainet.site"}
 	} else {
-		config.AllowAllOrigins = true
+		// config.AllowAllOrigins = true
+		config.AllowOrigins = []string{"http://apitest.ainet.site"}
+
 		// 测试环境下模糊匹配本地开头的请求
 		// config.AllowOriginFunc = func(origin string) bool {
 		// 	if regexp.MustCompile(`^http://127\.0\.0\.1:\d+$`).MatchString(origin) {
