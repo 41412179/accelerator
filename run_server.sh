2170eeed4c407b5d605de3b79e873ed95b10e6ea
dannygguo <dannygguo@tencent.com>
2022-05-31 21:27:52 +0800

    fix script

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,10 @@
 set -x
 git pull
 go build
-/bin/kill $(ps -ef | grep "./accelerator" | awk '{print $2}')
-nohup ./accelerator >acc.log 2>&1 &
+pid=$(ps -ef | grep "./accelerator" | grep -v 'grep' | awk '{print $2}')
+if [ $pid -gt 0 ]; then
+    /bin/kill -9 $pid
+fi
+
+nohup ./accelerator >>acc.log 2>&1 &
+tail -200f acc.log
